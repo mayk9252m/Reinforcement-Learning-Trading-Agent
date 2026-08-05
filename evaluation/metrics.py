90231ca72a212f4bc243e6e8786fc679f406af20
Mayank Shrivastava <mayankshrivastava179@gmail.com>
2026-08-05 20:55:13 +0530

    adding the total_return

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -11,3 +11,8 @@ def returns_from_equity(equity_curve: list[float] | np.ndarray | pd.Series) -> p
     equity = pd.Series(equity_curve, dtype="float64")
     return equity.pct_change().replace([np.inf, -np.inf], np.nan).dropna()
 
+
+def total_return(equity_curve) -> float:
+    equity = pd.Series(equity_curve, dtype="float64")
+    return float(equity.iloc[-1] / equity.iloc[0] - 1)
+
