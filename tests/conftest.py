5222e16dcd7f20fe429a68ecbfc6226caae1a7ce
Mayank Shrivastava <mayankshrivastava179@gmail.com>
2026-08-11 21:59:07 +0530

    Working on the conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,12 @@ import numpy as np
 import pandas as pd
 
 
+@pytest.fixture
+def sample_data():
+    return pd.DataFrame({
+        "open": [100, 101, 102],
+        "high": [105, 106, 107],
+        "low": [95, 96, 97],
+        "close": [102, 103, 104],
+        "volume": [1000, 1100, 1200]
+    })
